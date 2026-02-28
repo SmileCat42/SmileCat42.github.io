af0eed8b98aa0e5a235df7cb0df95f6943fe2e96
Golfring <golfring.dove@hotmail.com>
2026-02-28 15:28:41 +0700

    add 1.npm i aos[animation] 2.npm farmer motion 3.npm i lucide-react[icon]

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: 'SmileCat42.github.io',   // สำคัญมาก
+  plugins: [react(), tailwindcss()],
+  base: '/', 
 })
