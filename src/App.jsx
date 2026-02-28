9a178f842d73457afa2db61048d6787b17c0c314
Golfring <golfring.dove@hotmail.com>
2026-02-28 12:18:06 +0700

    start portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-200">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Tailwind v4 ทำงานแล้ว 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
