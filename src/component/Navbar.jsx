34cb90eba9f9db51ebdcbc0f0476ede019869fe6
Golfring <golfring.dove@hotmail.com>
2026-03-01 13:40:03 +0700

    start to build mobile button mode

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import { motion} from "framer-motion";
 import { useState } from "react"
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, Menu, X } from "lucide-react";
 
 const Navbar = ({ darkMode, toggleDarkMode}) => {
   const [activeSection, setActiveSection] = useState('home')
@@ -107,6 +107,38 @@ const Navbar = ({ darkMode, toggleDarkMode}) => {
                   <Sun className="w-5 h-5 text-yellow-300"/>
                 ) : (
                   <Moon className="w-5 h-5 text-gray-700"/>
+                )}
+            </motion.button>
+            {/* Button */}
+            <motion.a
+            href="#content"
+            whileHover={{ scale: 1.05}}
+            whileTap={{scale:0.95}}
+            className={`hidden lg:block px-6 py-2 font-semibold
+            rounded-full bg-linear-to-r ${colors.button}
+            text-white shadow-md hover:shadow-lg transition-shadow`}>
+              Hire Me
+            </motion.a>
+          </div>
+          {/* Mobile Menu but */}
+          <div className="flex lg:hidden items-center space-x-4 px-2">
+            <motion.button
+            whileTap={{ scale: 0.9}}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className={`p-2 rounded-lg ${darkMode
+              ? 'bg-gray-700'
+              : 'bg-gray-200'
+            }`}>
+                {isMenuOpen ? (
+                  <X className={`w-5 h-5 ${darkMode
+                     ? 'text-white'
+                     : 'text-gray-700'
+                    }`}/>
+                ) : (
+                    <Menu className={`w-5 h-5 ${darkMode
+                      ? 'text-white'
+                      : 'text-gray-700'
+                    }`}/>
                 )}
             </motion.button>
           </div>
